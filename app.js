48591ad0f0cefd2b8c0e0e5bbd3e6f450c92667d
PeterOhue <39671380+PeterOhue@users.noreply.github.com>
2021-01-09 20:47:26 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+document.querySelector('#red').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'red';
+}
+document.querySelector('#orange').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'orange';
+}
+document.querySelector('#yellow').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'yellow';
+}
+document.querySelector('#green').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'green';
+}
+document.querySelector('#blue').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'blue';
+}
+document.querySelector('#green').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'green';
+}
+document.querySelector('#indigo').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'indigo';
+}
+document.querySelector('#purple').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'purple';
+}
+document.querySelector('#pink').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'pink';
+}
+document.querySelector('#brown').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'rgb(192, 150, 100)';
+}
+document.querySelector('#grey').onclick = function () {
+	document.querySelector('body').style.backgroundColor = 'grey';
+}
